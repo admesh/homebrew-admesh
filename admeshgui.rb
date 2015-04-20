e5c33bbce8ce1eef039af6ce76bc4456491b2723
Miro Hrončok <miro@hroncok.cz>
2015-04-20 11:37:03 -0700

    Add admeshgui

---
+++ admeshgui.rb
@@ -0,0 +1,21 @@
+class Admeshgui < Formula
+  homepage "https://github.com/vyvledav/ADMeshGUI"
+  head "https://github.com/vyvledav/ADMeshGUI.git"
+
+  env :std
+
+  depends_on "admesh"
+  depends_on "stlsplit"
+  depends_on "qt5"
+  depends_on "gettext"
+
+  def install
+    system "qmake", "LIBS+=-L#{HOMEBREW_PREFIX}/Cellar/admesh/0.98.1/lib -L#{HOMEBREW_PREFIX}/Cellar/stlsplit/1.1/lib -L#{HOMEBREW_PREFIX}/opt/gettext/lib"
+    system "make"
+    bin.install "admeshgui.app/Contents/MacOS/admeshgui"
+  end
+
+  test do
+    system "true"
+  end
+end
