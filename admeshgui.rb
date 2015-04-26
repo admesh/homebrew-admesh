a6b9e541c26565a412bbcaab9b4caae5fb034e43
Miro Hrončok <miro@hroncok.cz>
2015-04-27 00:53:06 +0200

    Install the .app bundle

--- admeshgui.rb
+++ admeshgui.rb
@@ -12,7 +12,9 @@ class Admeshgui < Formula
   def install
     system "qmake", "LIBS+=-L#{HOMEBREW_PREFIX}/opt/admesh/lib -L#{HOMEBREW_PREFIX}/opt/stlsplit/lib -L#{HOMEBREW_PREFIX}/opt/gettext/lib", "QMAKE_CXXFLAGS+=-I#{HOMEBREW_PREFIX}/opt/gettext/include"
     system "make"
-    bin.install "admeshgui.app/Contents/MacOS/admeshgui"
+    system "ln", "-s", "../admeshgui.app/Contents/MacOS/admeshgui", "admeshgui"
+    prefix.install "admeshgui.app"
+    bin.install "admeshgui"
   end
 
   test do
